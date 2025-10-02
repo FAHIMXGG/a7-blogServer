d9c2acbf2eacc8686a969026e4c2c95db075a42d
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-10-02 15:49:55 +0600

    v re

--- api/health.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
-function handler(_req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200;
-    res.end(JSON.stringify({ ok: true, runtime: 'vercel-node' }));
-}
--- api/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
-const app_1 = require("../src/app");
-function handler(req, res) {
-    return (0, app_1.app)(req, res);
-}
