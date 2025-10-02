6c1bc55e734e4366547a8d4a8153f6f4c5f24936
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-10-02 15:41:57 +0600

    re ve

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,5 @@
-// api/index.ts
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { app } from '../src/app';
-
-// Express is a request handler already. We just delegate to it.
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return app(req, res);
 }
