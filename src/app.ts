2241ada8e664e31682896b5425f1b5f6a82b4b35
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-10-02 15:08:23 +0600

    vercel test

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(
   })
 );
 
-app.get('/health', (_req, res) => res.json({ ok: true }));
+app.get('/blogs', (_req, res) => res.json({ ok: true }));
 
 const apiPrefix = process.env.API_PREFIX ?? '/api';
 
